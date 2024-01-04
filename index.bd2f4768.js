0401557a6c4b6f21bf9ce7e3531628b980d48c36
Vovchuk <kulivackijvoloda@gmail.com>
2024-01-04 15:31:02 +0200

    make build

---
+++ index.bd2f4768.js
@@ -0,0 +1,3 @@
+function t(t){return t.toLocaleString("en-US")}var e=0,o=0,n=document.querySelector(".total-population"),r=document.querySelector(".average-population"),l=document.querySelectorAll(".population");l.forEach(function(t){var o=t.textContent.split(",").join("");e+=+o}),o=e/l.length,n.textContent=t(e),r.textContent=t(o);//# sourceMappingURL=index.bd2f4768.js.map
+
+//# sourceMappingURL=index.bd2f4768.js.map
